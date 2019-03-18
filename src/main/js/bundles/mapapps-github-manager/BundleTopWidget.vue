e20994432f190fbc7d9a509ebbf855d2e80df299
mste <m.stein@conterra.de>
2019-03-18 09:23:42 +0100

    Add eslintrc file and reformat code

--- src/main/js/bundles/mapapps-github-manager/BundleTopWidget.vue
+++ src/main/js/bundles/mapapps-github-manager/BundleTopWidget.vue
@@ -1,49 +1,75 @@
 <template>
     <div>
-        <div class="bundleTitle">{{i18n.highlightBundles}}</div>
-        <v-carousel v-if="bundles.length>0" :cycle=false :height="320">
-            <v-carousel-item v-for="(bundle, index) in bundles" :key="bundle.id">
+        <div class="bundleTitle">{{ i18n.highlightBundles }}</div>
+        <v-carousel
+            v-if="bundles.length>0"
+            :cycle="false"
+            :height="320">
+            <v-carousel-item
+                v-for="bundle in bundles"
+                :key="bundle.id">
                 <v-container class="bundleInfoContainer">
                     <v-card raised>
-                        <v-layout row wrap>
-                            <v-flex xs12 sm8 md6>
+                        <v-layout
+                            row
+                            wrap>
+                            <v-flex
+                                xs12
+                                sm8
+                                md6>
                                 <v-card-title>
                                     <div>
-                                        <h3 class="headline mb-0">{{bundle.name}}</h3>
+                                        <h3 class="headline mb-0">{{ bundle.name }}</h3>
                                     </div>
                                 </v-card-title>
                                 <v-card-text>
-                                    <div class="description">{{bundle.description}}</div>
-                                    <!--<div class="chips">
-                                        <v-chip v-for="topic in bundle.topics" :key="topic" disabled>{{topic}}</v-chip>
-                                    </div>-->
+                                    <div class="description">{{ bundle.description }}</div>
                                 </v-card-text>
-                                <v-spacer></v-spacer>
+                                <v-spacer/>
                                 <v-card-actions>
-                                    <v-btn @click="installBundle(bundle.id)" color="primary">
-                                        <v-icon class="hidden-sm-and-down" left>icon-download</v-icon>
-                                        <span class="hidden-sm-and-down">{{i18n.highlightBundleView.install}}</span>
+                                    <v-btn
+                                        color="primary"
+                                        @click="installBundle(bundle.id)">
+                                        <v-icon
+                                            class="hidden-sm-and-down"
+                                            left>icon-download
+                                        </v-icon>
+                                        <span class="hidden-sm-and-down">{{ i18n.highlightBundleView.install }}</span>
                                         <v-icon class="hidden-md-and-up">icon-download</v-icon>
                                     </v-btn>
-                                    <v-btn @click="openLink(bundle.homepage)" color="info">
-                                        <v-icon class="hidden-sm-and-down" left>icon-app-launch</v-icon>
-                                        <span class="hidden-sm-and-down">{{i18n.highlightBundleView.demo}}</span>
+                                    <v-btn
+                                        color="info"
+                                        @click="openLink(bundle.homepage)">
+                                        <v-icon
+                                            class="hidden-sm-and-down"
+                                            left>icon-app-launch
+                                        </v-icon>
+                                        <span class="hidden-sm-and-down">{{ i18n.highlightBundleView.demo }}</span>
                                         <v-icon class="hidden-md-and-up">icon-app-launch</v-icon>
                                     </v-btn>
-                                    <v-btn @click="openLink(bundle.html_url)" color="info">
-                                        <v-icon class="hidden-sm-and-down" left>launch</v-icon>
+                                    <v-btn
+                                        color="info"
+                                        @click="openLink(bundle.html_url)">
+                                        <v-icon
+                                            class="hidden-sm-and-down"
+                                            left>launch
+                                        </v-icon>
                                         <span
-                                            class="hidden-sm-and-down">{{i18n.highlightBundleView.openInGithub}}</span>
+                                            class="hidden-sm-and-down">{{ i18n.highlightBundleView.openInGithub }}</span>
                                         <v-icon class="hidden-md-and-up">launch</v-icon>
                                     </v-btn>
                                 </v-card-actions>
                             </v-flex>
-                            <v-flex xs12 sm4 md6 class="hidden-xs-and-down">
+                            <v-flex
+                                xs12
+                                sm4
+                                md6
+                                class="hidden-xs-and-down">
                                 <v-img
+                                    :src="bundle.html_url + '/raw/' + bundle.default_branch + '/' + 'screenshot.JPG'"
+                                    height="250px"
                                     @click="openLink(bundle.html_url + '/raw/' + bundle.default_branch + '/' + 'screenshot.JPG')"
-                                    v-bind:src="bundle.html_url + '/raw/' + bundle.default_branch + '/' + 'screenshot.JPG'"
-                                    height="250px">
-                                </v-img>
+                                />
                             </v-flex>
                         </v-layout>
                     </v-card>
@@ -58,21 +84,18 @@
 
     export default {
         mixins: [Bindable],
-        mounted: function () {
-            let highlightTopic = this.store.highlightTopic;
-            ct_when(this.store.query({}), (results) => {
-                this.bundles = results.filter((bundle) => {
-                    return !!bundle.topics.includes(highlightTopic);
-                });
-            });
-        },
-        components: {},
         data: function () {
             return {
                 i18n: {},
                 bundles: [],
                 selectedTag: null
             };
+        },
+        mounted: function () {
+            let highlightTopic = this.store.highlightTopic;
+            ct_when(this.store.query({}), (results) => {
+                this.bundles = results.filter((bundle) => !!bundle.topics.includes(highlightTopic));
+            });
         },
         methods: {
             openLink: function (url) {
