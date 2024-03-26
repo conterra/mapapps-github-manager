5781088bdfe85296d11949ffc1b8801182a07f03
Marko Reiprecht <m.reiprecht@conterra.de>
2024-03-26 19:28:58 +0100

    Use store-api and replace some old methods with new async/await or alternative helpers

--- src/main/js/bundles/mapapps-github-manager/BundleDetailsController.js
+++ src/main/js/bundles/mapapps-github-manager/BundleDetailsController.js
@@ -13,227 +13,210 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import d_lang from "dojo/_base/lang";
-import declare from "dojo/_base/declare";
-import _Connect from "ct/_Connect";
-import ct_when from "ct/_when";
 import Hash from "ct/Hash";
 import apprt_request from "apprt-request";
-import { SyncInMemoryStore } from "store-api/InMemoryStore";
+import { replace } from "apprt-core/string-replace";
 
-export default declare([_Connect], {
+export default class BundleDetailsController {
     // injected
-    dataformService: null,
-    windowManager: null,
-    store: null,
-    bCtx: null,
-    widgetDefinition: {
+    dataformService = null;
+    windowManager = null;
+    store = null;
+    bCtx = null;
+    widgetDefinition = {
         "dataform-version": "1.0.0",
         "showLabels": false,
         "size": {
-            "w": 380,
-            "h": 190
+            w: 380,
+            h: 190
         },
-        "children": [{
-            "type": "label",
-            "cssClass": "title",
-            "value": "${license}",
-            "size": {
-                "t": 0,
-                "l": 5
+        "children": [
+            {
+                type: "label",
+                cssClass: "title",
+                value: "${license}",
+                size: {
+                    t: 0,
+                    l: 5
+                }
+            },
+            {
+                type: "label",
+                cssClass: "title",
+                value: "${instructions}",
+                size: {
+                    t: 100,
+                    l: 5
+                }
+            },
+            {
+                type: "label",
+                value: "${version}",
+                size: {
+                    t: 145,
+                    l: 5
+                }
+            },
+            {
+                type: "selectbox",
+                field: "tagSelection",
+                searchable: false,
+                values: {
+                    field: "availableTags"
+                },
+                required: true,
+                size: {
+                    t: 140,
+                    l: 60
+                }
+            },
+            {
+                type: "button",
+                value: "${installButton}",
+                size: {
+                    t: 140,
+                    r: 5
+                }
             }
-        }, {
-            "type": "label",
-            "cssClass": "title",
-            "value": "${instructions}",
-            "size": {
-                "t": 100,
-                "l": 5
-            }
-        }, {
-            "type": "label",
-            "value": "${version}",
-            "size": {
-                "t": 145,
-                "l": 5
-            }
-        }, {
-            "type": "selectbox",
-            "field": "tagSelection",
-            "searchable": false,
-            "store": "tagsStore",
-            "required": true,
-            "size": {
-                "t": 140,
-                "l": 60
-            }
-        }, {
-            "type": "button",
-            "value": "${installButton}",
-            "size": {
-                "t": 140,
-                "r": 5
-            }
-        }]
-    },
+        ]
+    };
 
-    /**
-     * @constructs
-     */
-    constructor: function () {
-    },
-
-    activate: function (ctx) {
-        const i18n = this.i18n = this._i18n.get().bundleDetailsView;
+    activate(ctx) {
+        const i18n = (this.i18n = this._i18n.get().bundleDetailsView);
         this.effectiveWidgetDefinition = this._substituteWidgetDefinition(this.widgetDefinition, i18n);
         this.bCtx = ctx.getBundleContext();
-    },
+    }
 
-    deactivate: function () {
+    deactivate() {
         this.effectiveWidgetDefinition = null;
         this.i18n = null;
         this.disconnect();
-    },
+    }
 
-    showDetails: function (params) {
+    async showDetails(params) {
         const i18n = this.i18n;
-        const id = params.getProperty && params.getProperty("id") || params.id;
+        const id = (params.getProperty && params.getProperty("id")) || params.id;
         if (!id) {
             return;
         }
-        const item = this.store.getBundleDetails(id);
-        if (item.length && item[0].has_downloads) {
-            this.currentItem = item[0];
-            this._createWindow(this.currentItem, i18n);
+        const item = await this.store.get(id);
+        if (item?.has_downloads) {
+            this._createWindow(item, i18n);
         } else {
-
-            const w = this.detailWindow = this.windowManager.createModalWindow({
+            const w = (this.detailWindow = this.windowManager.createModalWindow({
                 title: i18n.noReleasesTitle,
                 draggable: true,
                 dndDraggable: false,
                 content: i18n.noReleasesYet,
                 closable: true
-            });
+            }));
             w.show();
         }
-    },
+    }
 
-    _createWindow: function (item, i18n) {
-        const tagsUrl = "https://api.github.com/repos/conterra/" + item.name + "/releases";
-        const id = "tagsStore";
+    async _lookupAvailableTags(repositoryName) {
+        const tagsUrl = "https://api.github.com/repos/conterra/" + repositoryName + "/releases";
+        try {
+            const response = await apprt_request(tagsUrl, { jsonp: true });
+            const releases = response.data.filter((release) => !release.name.includes("SNAPSHOT"));
+            releases.sort((a, b) =>
+                b.name.replace(/\d+/g, (n) => +n + 100000).localeCompare(a.name.replace(/\d+/g, (n) => +n + 100000))
+            );
+            return releases.map((r) => r.name);
+        } catch (e) {
+            console.error("Could not fetch tags from " + tagsUrl, e);
+            return [];
+        }
+    }
 
-        ct_when(apprt_request(tagsUrl, {jsonp: true}), function (response) {
+    async _createWindow(item, i18n) {
+        const tags = await this._lookupAvailableTags(item.name);
 
-            let content;
+        let content;
 
-            if (response.data.length) {
-                const releases = response.data.filter((release) => !release.name.includes("SNAPSHOT"));
-                releases.sort((a, b) => b.name.replace(/\d+/g, n => +n + 100000)
-                    .localeCompare(a.name.replace(/\d+/g, n => +n + 100000)));
-
-                const tagsStore = new SyncInMemoryStore({
-                    id: id,
-                    idProperty: "name",
-                    data: releases // version contained here
-                });
-
-                this.bCtx.registerService(["ct.api.Store"], tagsStore, {
-                    id: id,
-                    useIn: ["dataform"]
-                });
-
-                const widget = this._createWidget(item, i18n);
-
-                widget.connect(widget, "onControlEvent", d_lang.hitch(this, function (evt, foo) {
-
-                    const buttonWidget = this.buttonWidget = evt.control._getWidget();
-                    buttonWidget.set("disabled", true);
-                    buttonWidget.set("iconClass", "icon-spinner");
-
-                    this._triggerAddBundleProcess();
-
-                }));
-                content = widget;
-
-            } else {
-                content = i18n.noReleasesYet;
-            }
-            const w = this.detailWindow = this.windowManager.createModalWindow({
-                title: d_lang.replace(i18n.windowTitle, item),
-                draggable: true,
-                dndDraggable: false,
-                content: content,
-                closable: true
+        if (tags.length) {
+            const widget = this._createWidget(item, tags);
+            widget.on("ControlEvent", (evt) => {
+                const buttonWidget = (this.buttonWidget = evt.control._getWidget());
+                buttonWidget.set("disabled", true);
+                buttonWidget.set("iconClass", "icon-spinner");
+                this._triggerAddBundleProcess(widget);
             });
-            w.show();
-        }, this);
+            content = widget;
+        } else {
+            content = i18n.noReleasesYet;
+        }
+        const w = (this.detailWindow = this.windowManager.createModalWindow({
+            title: replace(i18n.windowTitle, item),
+            draggable: true,
+            dndDraggable: false,
+            content: content,
+            closable: true
+        }));
+        w.show();
+    }
 
-
-    },
-
-    _createWidget: function (item, i18n) {
+    _createWidget(item, tags) {
         const dfService = this.dataformService;
-        const form = this.form = dfService.createDataForm(this.effectiveWidgetDefinition);
+        const form = dfService.createDataForm(this.effectiveWidgetDefinition);
         const binding = dfService.createBinding("object", {
-            data: item
+            data: {
+                tagSelection: null,
+                availableTags: tags,
+                item
+            }
         });
         form.set("dataBinding", binding);
-
         return form;
-    },
+    }
 
-    _substituteWidgetDefinition: function (widgetDefinition, params) {
+    _substituteWidgetDefinition(widgetDefinition, params) {
         return new Hash(widgetDefinition).substitute(params, true).asMap();
-    },
+    }
 
-    _triggerAddBundleProcess: function () {
-        const item = this.currentItem;
-        const binding = this.form.get("dataBinding");
-        let tag;
-        if (binding) {
-            tag = binding.data.tagSelection;
+    async _triggerAddBundleProcess(form) {
+        const binding = form.get("dataBinding");
+        const tag = binding.data.tagSelection;
+        const item = binding.data.item;
+
+        const url =
+            "https://github.com/conterra/" + item.name + "/releases/download/" + tag + "/" + item.name + "-bundle.zip";
+        this.buttonWidget.set("label", this.i18n.downloading);
+
+        try {
+            const blob = await this._downloadArchive(url);
+            await this._uploadBundle(blob, item.name);
+        } catch (e) {
+            this.detailWindow.set("content", this.i18n.notFound);
         }
-        if (tag) {
-            const url = "https://github.com/conterra/" + item.name
-                + "/releases/download/" + tag + "/"
-                + item.name + "-bundle.zip";
-            this.buttonWidget.set("label", this.i18n.downloading);
+    }
 
-
-            const downloadRequest = this._downloadArchive(url);
-            ct_when(downloadRequest, this._uploadBundle, function (error) {
-                this.detailWindow.set("content", this.i18n.notFound);
-            }, this);
-        }
-    },
-
-    _downloadArchive: function (url) {
+    _downloadArchive(url) {
         return apprt_request(url, {
             handleAs: "blob"
         });
-    },
+    }
 
-    _uploadBundle: function (blob) {
+    async _uploadBundle(blob, itemName) {
         this.buttonWidget.set("label", this.i18n.uploading);
-        const fileName = this.currentItem.name + ".zip";
+        const fileName = itemName + ".zip";
         const url = this._properties.uploadTarget;
         const formData = new FormData();
-        formData.append('file', blob, fileName);
+        formData.append("file", blob, fileName);
         formData.append("f", "json");
-        const uploadRequest = apprt_request.post(url, {
-            data: formData
-        });
-
-        ct_when(uploadRequest, function (res) {
+        try {
+            await apprt_request.post(url, {
+                data: formData
+            });
             this.buttonWidget.set("disabled", false);
             this.buttonWidget.set("iconClass", "icon-sign-success");
             this.buttonWidget.set("class", "input-success");
             this.buttonWidget.set("label", this.i18n.integrationSuccess);
-            setTimeout(d_lang.hitch(this, function () {
-                this.detailWindow.close()
-            }), 1500);
-        }, function (e) {
+            setTimeout(() => {
+                this.detailWindow.close();
+            }, 1500);
+        } catch (e) {
             this.detailWindow.set("content", this.i18n.integrationFailed);
-        }, this);
+        }
     }
-});
+}
